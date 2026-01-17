ea38bd25f1566e3d12c80796bb4f3d21950029e3
Khanon Curtis <khanon.boyd@gmail.com>
2026-01-17 11:04:07 -0700

    format files

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,33 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
     config = function()
-      require("nvim-treesitter").setup({
+      require('nvim-treesitter').setup {
         ensure_installed = {
-          "bash",
-          "c",
-          "cpp",
-          "diff",
-          "html",
-          "css",
-          "lua",
-          "luadoc",
-          "markdown",
-          "markdown_inline",
-          "query",
-          "vim",
-          "vimdoc",
-          "rust",
-          "python",
-          "typescript",
-          "javascript",
+          'bash',
+          'c',
+          'cpp',
+          'diff',
+          'html',
+          'css',
+          'lua',
+          'luadoc',
+          'markdown',
+          'markdown_inline',
+          'query',
+          'vim',
+          'vimdoc',
+          'rust',
+          'python',
+          'typescript',
+          'javascript',
         },
         auto_install = true,
-      })
+      }
 
       -- Enable treesitter-based highlighting and indentation
-      vim.api.nvim_create_autocmd("FileType", {
+      vim.api.nvim_create_autocmd('FileType', {
         callback = function()
           pcall(vim.treesitter.start)
         end,
