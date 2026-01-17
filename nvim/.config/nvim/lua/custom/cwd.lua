a89173c78244dab0f01ea6b15f67dd954383501d
Khanon Curtis <khanon.boyd@gmail.com>
2026-01-16 20:29:16 -0700

    update cwd.lua

--- nvim/.config/nvim/lua/custom/cwd.lua
+++ nvim/.config/nvim/lua/custom/cwd.lua
@@ -1,6 +1,18 @@
--- automatically set window-local cwd to current file's folder
 vim.api.nvim_create_autocmd('BufEnter', {
   callback = function()
-    vim.cmd 'lcd %:p:h'
+    -- skip non-file buffers (terminal, floating, help, etc.)
+    if vim.bo.buftype ~= '' then
+      return
+    end
+
+    local file = vim.fn.expand '%:p'
+    if file == '' then
+      return
+    end
+
+    local dir = vim.fn.fnamemodify(file, ':h')
+    if vim.fn.isdirectory(dir) == 1 then
+      vim.cmd('lcd ' .. vim.fn.fnameescape(dir))
+    end
   end,
 })
